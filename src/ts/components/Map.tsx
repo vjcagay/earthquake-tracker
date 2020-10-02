3ce8697f021a32d1f5f8683507ec42eb8cb8eeb0
Vincent John Cagay <me@vjcagay.com>
2020-10-02 16:07:06 +0900

    Implement map markers for each feature

--- src/ts/components/Map.tsx
+++ src/ts/components/Map.tsx
@@ -1,18 +1,21 @@
-import MapboxGL from "mapbox-gl";
-import React, { useLayoutEffect, useRef } from "react";
+import MapboxGL, { Map as MapboxMap, Marker } from "mapbox-gl";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 interface Props {
   className?: string;
+  features?: Feature[];
 }
 
 MapboxGL.accessToken = __MAP_ACCESS_TOKEN__;
+
+let map: MapboxMap;
 
 const Map = (props: Props) => {
   const container = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    const map = new MapboxGL.Map({
+    map = new MapboxMap({
       container: container.current,
       style: "mapbox://styles/mapbox/dark-v10",
     });
@@ -36,8 +39,30 @@ const Map = (props: Props) => {
       "bottom-right"
     );
 
-    return () => map.remove();
-  });
+    return () => map?.remove();
+  }, []);
+
+  const [markers, setMarkers] = useState<Marker[]>([]);
+
+  // Convert the features into markers that Mapbox understands
+  useEffect(() => {
+    if (map) {
+      const newMarkers = props.features.map((feature) => {
+        const [longitude, latitude] = feature.geometry.coordinates;
+        const marker = new Marker().setLngLat([longitude, latitude]);
+        return marker;
+      });
+
+      setMarkers(newMarkers);
+    }
+  }, [props.features]);
+
+  useEffect(() => {
+    markers.forEach((marker) => marker.addTo(map));
+
+    // For each new set of markers, remove old ones
+    return () => markers.forEach((marker) => marker.remove());
+  }, [markers]);
 
   return <div className={props.className} ref={container} />;
 };
