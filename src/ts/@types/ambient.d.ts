0f65a3daadbec7400ea67d2001e950d7b62b7692
Vincent John Cagay <me@vjcagay.com>
2020-10-02 16:05:38 +0900

    Add Feature and FeatureCollection types

--- src/ts/@types/ambient.d.ts
+++ src/ts/@types/ambient.d.ts
@@ -1,1 +1,52 @@
 declare var __MAP_ACCESS_TOKEN__: string;
+
+interface Feature {
+  geometry: {
+    coordinates: number[];
+    type: "Point";
+  };
+  id: string;
+  properties: {
+    alert?: string;
+    cdi?: number;
+    code: string;
+    detail: string;
+    dmin: number;
+    felt?: number;
+    gap: number;
+    ids: string;
+    mag: number;
+    magType: string;
+    mmi?: number;
+    net: string;
+    nst?: number;
+    place: string;
+    rms: number;
+    sig: number;
+    sources: string;
+    status: string;
+    time: number;
+    title: string;
+    tsunami: 0;
+    type: string;
+    types: string;
+    tz: number;
+    updated: number;
+    url: string;
+  };
+  type: "Feature";
+}
+
+interface FeatureCollection {
+  bbox: number[];
+  features: Feature[];
+  metadata: {
+    api: string;
+    count: number;
+    generated: number;
+    status: number;
+    title: "USGS Earthquakes";
+    url: string;
+  };
+  type: "FeatureCollection";
+}
